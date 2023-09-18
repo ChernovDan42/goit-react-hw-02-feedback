cd106ee5e2910ad8b18c17619228161e1168cf34
Dan Chernov <120645560+ChernovDan42@users.noreply.github.com>
2023-09-18 15:37:33 +0200

    PropTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,93 +1,74 @@
-import { Component } from "react";
-import { Statistics } from "./Statistics/Statistics";
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { Section } from "./Section/Section";
-import { Notification } from "./Notifications/Notification";
-import css from './App.module.css'
-
-
-
-
+import { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notifications/Notification';
+import css from './App.module.css';
 
 export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-}
-  
-  
-  handleClick = (e) => {
+  handleClick = e => {
     const { name } = e.target;
-    
 
     this.setState(prevState => ({
-        [name]: prevState[name] + 1
+      [name]: prevState[name] + 1,
     }));
-}
+  };
 
-  
   countTotalFeedback() {
     const { good, neutral, bad } = this.state;
-    
-    return good+neutral+bad
+
+    return good + neutral + bad;
   }
 
-
   countPositiveFeedbackPercentage() {
-    const {good} = this.state;
+    const { good } = this.state;
 
     const percentageGoodReviews = (good / this.countTotalFeedback()) * 100;
     if (this.countTotalFeedback() > 0) {
       return percentageGoodReviews.toFixed(2);
     }
-    return '0.00'
-    
+    return '0.00';
   }
 
-
-  
-  
-  
   render() {
-const { good, neutral, bad } = this.state;
+    const { good, neutral, bad } = this.state;
 
     const stateLength = Object.keys(this.state);
-   
-    
+
     return (
       <div className={css.App}>
-        <Section title='Please leave feedback'>
-        {stateLength.map(el => {
-          return (
-            <FeedbackOptions key={el}
-              options={el}
-              onLeaveFeedback={this.handleClick}
+        <Section title="Please leave feedback">
+          {stateLength.map(option => {
+            return (
+              <FeedbackOptions
+                key={option}
+                options={option}
+                onLeaveFeedback={this.handleClick}
+              />
+            );
+          })}
+        </Section>
+
+        <Section title="Statistics">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
             />
-          )
-        })}
-          </Section>
-
-
-
-        <Section title='Statistics'>
-
-         {this.countTotalFeedback() ? <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-            : <Notification 
-               message="No feedback given"/>}
-
-        
-          </Section>
-      
-</div>
- )
-}
-
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
+      </div>
+    );
+  }
 }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,14 +1,24 @@
-import css from "./FeedbackOptions.module.css"
-
+import css from './FeedbackOptions.module.css';
+import PropTypes from 'prop-types';
 
 function capitalizeFirstLetter(options) {
   return options.charAt(0).toUpperCase() + options.slice(1);
 }
 
-
 export function FeedbackOptions({ options, onLeaveFeedback }) {
-    return (
-         <button className={css.btn} type="button" name={options} onClick={onLeaveFeedback}>{capitalizeFirstLetter(options)}</button>
-    )
-   
+  return (
+    <button
+      className={css.btn}
+      type="button"
+      name={options}
+      onClick={onLeaveFeedback}
+    >
+      {capitalizeFirstLetter(options)}
+    </button>
+  );
 }
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.string.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
